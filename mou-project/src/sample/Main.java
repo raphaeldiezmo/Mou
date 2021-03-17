885d1d10c83300b6a69c1eeb916fba7ede34bb7d
John Diezmo <john.diezmo@mycit.ie>
2021-03-17 21:21:59 +0000

    adding initial mou-app (JavaFX initial design)

---
+++ mou-project/src/sample/Main.java
@@ -0,0 +1,26 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("mou.fxml"));
+        primaryStage.setTitle("Mou");
+        primaryStage.getIcons().add(new Image(("file: icon.ico")));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ mou-project/src/sample/UploadItems.java
@@ -0,0 +1,13 @@
+package sample;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class UploadItems extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
